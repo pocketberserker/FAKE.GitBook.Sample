d6a488abf30ff1e5005540fa72c33e46ec35df93
pocketberserker <pocketberserker@gmail.com>
2016-01-02 06:55:07 +0900

    fix scripts

--- build.sh
+++ build.sh
@@ -21,11 +21,17 @@ function run() {
   fi
 }
 
+run .paket/paket.bootstrapper.exe
+
 if [[ "$OS" != "Windows_NT" ]] &&
        [ ! -e ~/.config/.mono/certs ]
 then
   mozroots --import --sync --quiet
 fi
 
+run .paket/paket.exe restore
+
+[ ! -e build.fsx ] && run .paket/paket.exe update
+[ ! -e build.fsx ] && run packages/FAKE/tools/FAKE.exe init.fsx
 run packages/FAKE/tools/FAKE.exe "$@" $FSIARGS build.fsx
 
