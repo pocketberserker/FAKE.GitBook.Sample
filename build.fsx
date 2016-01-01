b93c72de0a5704c30f790914af033b885c98b862
pocketberserker <pocketberserker@gmail.com>
2016-01-02 07:40:42 +0900

    add release task

--- build.fsx
+++ build.fsx
@@ -6,8 +6,23 @@
 #r @"packages/FAKE.GitBook/lib/net451/Fake.GitBook.dll"
 open Fake
 
+let gitOwner = "pocketberserker"
+let gitHome = "https://github.com/" + gitOwner
+let gitName = "FAKE.GitBook.Sample"
+
 Target "Generate" (fun _ ->
   GitBook id (fun p -> { p with SrcDir = currentDirectory @@ "src" }) Html
+)
+
+Target "Release" (fun _ ->
+    let tempDocsDir = "temp/gh-pages"
+    CleanDir tempDocsDir
+    Repository.cloneSingleBranch "" (gitHome + "/" + gitName + ".git") "gh-pages" tempDocsDir
+
+    CopyRecursive "gitbook/_book" tempDocsDir true |> tracefn "%A"
+    StageAll tempDocsDir
+    Git.Commit.Commit tempDocsDir (sprintf "auto commit on AppVeyor %s" (environVar "APPVEYOR_BUILD_NUMBER"))
+    Branches.push tempDocsDir
 )
 
 RunTargetOrDefault "Generate"
