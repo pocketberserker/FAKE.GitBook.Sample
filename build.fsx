35152d0e2b0f8a99574e7e45e0bc4fb94aa87276
pocketberserker <pocketberserker@gmail.com>
2016-01-11 13:28:02 +0900

    remove unnecessary code

--- build.fsx
+++ build.fsx
@@ -1,5 +1,4 @@
 #r @"packages/FAKE/tools/FakeLib.dll"
-#I @"packages/FAKE.GitBook/lib/net451"
 #I "packages/FSharp.Formatting/lib/net40"
 #I "packages/FSharp.Compiler.Service/lib/net40"
 #I "packages/FSharpVSPowerTools.Core/lib/net45"
